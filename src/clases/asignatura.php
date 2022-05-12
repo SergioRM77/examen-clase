83eaf55ed3a2d47e6fb440f9908677be8aa678a9
SergioRM77 <sergio93rm@gmail.com>
2022-05-12 12:16:23 +0200

    añadir creacion asignatura e ID

--- src/clases/asignatura.php
+++ src/clases/asignatura.php
@@ -2,6 +2,20 @@
 namespace ITEC\DAW\examen;
 
 class asignatura{
+    private string $titulo;
+    private static int $IDasignatura;
+
+    public function __construct(string $titulo)
+    {
+        $this->titulo = $titulo;
+        $this->ID = self::$IDasignatura++;
+    }
+
+    public static function create(string $titulo){
+        return new asignatura($titulo);
+    }
+
     
+
 }
 ?>
