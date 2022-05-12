9fb071e7526248ea402bca508f2cef0f5ac74bda
SergioRM77 <sergio93rm@gmail.com>
2022-05-12 12:43:11 +0200

    añadido incompleto a examen

---
+++ src/clases/examen.php
@@ -0,0 +1,37 @@
+<?php
+namespace ITEC\DAW\examen;
+
+use ITEC\DAW\examen\fecha;
+use ITEC\DAW\examen\hora;
+use ITEC\DAW\examen\pregunta;
+use ITEC\DAW\examen\listadopreguntas;
+use ITEC\DAW\examen\asignatura;
+use ITEC\DAW\examen\profesor;
+
+class examen{
+    private fecha $fecha;
+    private hora $hora;
+    private listadopreguntas $listapreguntas;
+    private asignatura $asignatura;
+    private profesor $profesor;
+
+
+    public function __construct(
+        fecha $fecha, hora $hora, listadopreguntas $listapreguntas,
+        asignatura $asignatura, profesor $profesor
+    )
+    {
+        $this->fecha = $fecha;
+        $this->hora = $hora;
+        $this->listapreguntas = $listapreguntas;
+        $this->asignatura = $asignatura;
+        $this->profesor = $profesor;
+    }
+
+    public static function create(fecha $fecha, hora $hora, listadopreguntas $listapreguntas,
+    asignatura $asignatura, profesor $profesor){
+        return new examen($fecha, $hora, $listapreguntas, $asignatura, $profesor);
+    }
+}
+
+?>
